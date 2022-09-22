98e8fac6b2a1ed236c53dc55d7982426bc76bf15
Kovacs Sandor <sada93@citromail.hu>
2022-09-22 23:17:34 +0200

    feature/TL-6 Added form to Form.js and designed it

--- src/Components/TodoList/Form/Form.js
+++ src/Components/TodoList/Form/Form.js
@@ -1,5 +1,28 @@
 const Form = () => {
-  return;
+  return (
+    <div className="control">
+      <h3>My Todo's</h3>
+      <form>
+        <input placeholder="Add new..."></input>
+        <input type="date"></input>
+        <button className="add-btn">Add</button>
+      </form>
+      <div className="filter-sort">
+        <p className="filter__title">Filter:</p>
+        <select className="filter__select">
+          <option>Option1</option>
+          <option>Option2</option>
+          <option>Option3</option>
+        </select>
+        <p className="sort__title">Sort:</p>
+        <select className="sort__select">
+          <option>Option1</option>
+          <option>Option2</option>
+          <option>Option3</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
