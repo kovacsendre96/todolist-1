3097c982ff8eebb8bd3ff1292ba409daae09325a
Kovacs Sandor <sada93@citromail.hu>
2022-09-24 14:01:41 +0200

    feature/TL-7 created Form.scss and Button.scss, added global variables and classes to app.css

--- src/Components/TodoList/Form/Form.js
+++ src/Components/TodoList/Form/Form.js
@@ -1,25 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortAmountDownAlt } from '@fortawesome/free-solid-svg-icons'
+
+
 const Form = () => {
   return (
-    <div className="control">
+    <div className="flex-center control">
       <h3>My Todo's</h3>
       <form>
-        <input placeholder="Add new..."></input>
-        <input type="date"></input>
-        <button className="add-btn">Add</button>
+        <input className="margin-right-md" type="text" placeholder="Add new..."></input>
+        <input className="margin-right-sm" type="date"></input>
+        <button className="add-btn" type="submit">
+          Add
+        </button>
       </form>
-      <div className="filter-sort">
-        <p className="filter__title">Filter:</p>
-        <select className="filter__select">
+      <div className="filter-sort flex-center ">
+        <p className="margin-right-sm">Filter:</p>
+        <select className="margin-right-md">
           <option>Option1</option>
           <option>Option2</option>
           <option>Option3</option>
         </select>
-        <p className="sort__title">Sort:</p>
-        <select className="sort__select">
+        <p className="margin-right-sm">Sort:</p>
+        <select className="margin-right-sm">
           <option>Option1</option>
           <option>Option2</option>
           <option>Option3</option>
         </select>
+        <FontAwesomeIcon className='my-fav-icons margin-right-lg' icon={faSortAmountDownAlt} />
       </div>
     </div>
   );
