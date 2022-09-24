a6f13f5aa37ee75f6aa894b9bb66e19bad4e1695
Kovacs Sandor <sada93@citromail.hu>
2022-09-24 17:16:45 +0200

    feature/TL-8 almost done

--- src/Components/TodoList/List/List.js
+++ src/Components/TodoList/List/List.js
@@ -1,7 +1,12 @@
 import ListItem from "./ListItem";
 
 const List = () => {
-  return <ListItem />;
+  return (
+    <>
+      <ListItem title={"Főzés"} date={"2022.10.15"} status={'succes'} />
+      <ListItem title={"Mosogatás"} date={"2022.10.16"} status={'progress'} />
+    </>
+  );
 };
 
 export default List;
--- src/Components/TodoList/List/ListItem.js
+++ src/Components/TodoList/List/ListItem.js
@@ -1,5 +1,30 @@
-const ListItem = () => {
-  return;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const ListItem = ({ title, date, status }) => {
+  return (
+    <div className="flex-center card todo">
+      <div>{title}</div>
+      <div>
+        <div className="flex-center settings">
+          <button>
+            <FontAwesomeIcon icon={faCheck} />
+          </button>
+          <button>
+            Edit
+            <FontAwesomeIcon icon={faPencil} />
+          </button>
+          <button>
+            Delete
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </div>
+        <div>{date}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ListItem;
